9806b6c1d26b053ecd3c43b09d4a63bd1a72c0d7
AthulMathew2003 <mathewathul03@gmail.com>
2025-07-12 23:54:54 +0530

    Configured for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// Replace this with your repo name
-const repoName = 'todo-list'
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${todo-list}/`, // 👈 important
+  base: '/todo-list/', // 👈 use your GitHub repo name here
 })
